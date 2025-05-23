87424ad5af1621c86a7c62a2184ad8c963903818
Otavio <otaviofina@gmail.com>
2025-05-22 21:26:06 -0300

    2

--- src/app/components/estrelinhas/estrelinhas.component.ts
+++ src/app/components/estrelinhas/estrelinhas.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-estrelinhas',
-  imports: [],
   templateUrl: './estrelinhas.component.html',
-  styleUrl: './estrelinhas.component.scss'
+  styleUrls: ['./estrelinhas.component.scss'] ,
 })
 export class EstrelinhasComponent {
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private readonly router: Router){}
 
+  navigateBalatro(){
+    this.router.navigate(['IGT'])
+  }
+
+  navigateBalloon(){
+    this.router.navigate(['BART'])
+  }
 }
